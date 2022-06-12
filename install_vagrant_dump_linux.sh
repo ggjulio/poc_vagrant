a54939cc9e45a1e314198006255eb3768c051249
Julio GONZALEZ GOMEZ <juligonz@e2r5p13.clusters.42paris.fr>
2022-06-12 16:02:11 +0200

    poc

---
+++ install_vagrant_dump_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eu
+
+VAGRANT="https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_linux_amd64.zip"
+
+mkdir -p ~/bin
+wget -qO- "$VAGRANT" | busybox unzip -d ~/bin/ - 1> /dev/null
+chmod +x ~/bin/vagrant
+vagrant version
