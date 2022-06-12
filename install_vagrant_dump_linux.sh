ade9e63191544ead514281496b0830f582bddc72
ggjulio <juligonz@student.42.fr>
2022-06-12 16:08:28 +0200

    poc

--- install_vagrant_dump_linux.sh
+++ install_vagrant_dump_linux.sh
@@ -7,4 +7,5 @@ VAGRANT="https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_linux_amd6
 mkdir -p ~/bin
 wget -qO- "$VAGRANT" | busybox unzip -d ~/bin/ - 1> /dev/null
 chmod +x ~/bin/vagrant
+vagrant autocomplete install 1> /dev/null
 vagrant version
