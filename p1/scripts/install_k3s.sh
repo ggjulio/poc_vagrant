5a2ce8a5e3f2e9589c904733fe3969b2d0488817
ggjulio <juligonz@student.42.fr>
2022-06-24 19:37:38 +0200

    .

--- p1/scripts/install_k3s.sh
+++ p1/scripts/install_k3s.sh
@@ -9,9 +9,9 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 dnf -y update
 
-echo "Installing k3S..."
-curl -sfL https://get.k3s.io | sh -
-echo "k3S installed."
+# echo "Installing k3S..."
+# curl -sfL https://get.k3s.io | sh -
+# echo "k3S installed."
 
 echo "Setting up kubectl completion..."
 # dnf install bash-completion
