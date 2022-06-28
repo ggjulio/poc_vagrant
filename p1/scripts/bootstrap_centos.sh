a2dd18f4f65eafb228b9ee40ed5b54e4abf282c7
ggjulio <juligonz@student.42.fr>
2022-06-28 21:20:27 +0200

    refacto p1

--- p1/scripts/bootstrap_centos.sh
+++ p1/scripts/bootstrap_centos.sh
@@ -6,6 +6,3 @@ echo "Setting up mirrors and dnf update..."
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 dnf -y update
-
-echo "Setting up kubectl completion..."
-/usr/local/bin/kubectl completion bash >> /etc/bash_completion.d/kubectl
