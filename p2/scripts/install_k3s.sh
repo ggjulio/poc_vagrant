6116b073bad030187de815fe58090b5966043cfe
ggjulio <juligonz@student.42.fr>
2022-07-03 16:19:35 +0200

    working but only at the second vagrant up

--- p2/scripts/install_k3s.sh
+++ p2/scripts/install_k3s.sh
@@ -24,17 +24,6 @@ if [ -z "$NODE_IP" ]; then
    log_error "No variable NODE_IP specified. Please specify it."
 fi
 
-# log_info "Setting up mirrors and dnf update..."
-# sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-# sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-dnf -y --disablerepo '*' --enablerepo=extras swap centos-linux-repos centos-stream-repos
-
-# dnf -y update
-# dnf -y install wget kernel-headers kernel-devel-$(uname -r) kernel-header-$(uname -r)
-dnf -y install wget kernel-headers kernel-devel-$(uname -r) kernel-header-$(uname -r)
-# dnf -y install wget kernel-devel.x86_64
-# dnf -y group install "Development Tools"
-
 log_info "Installing server k3S with ${NODE_IP} as node ip..."
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--cluster-init --node-ip=${NODE_IP} --write-kubeconfig-mode 644" sh -
 log_info "k3S server node installed."
@@ -42,24 +31,12 @@ log_info "k3S server node installed."
 log_info "Setting up kubectl completion..."
 /usr/local/bin/kubectl completion bash >> /etc/bash_completion.d/kubectl
 
-# log_info "is VBox guest additions installed ?"
-# lsmod | grep vboxguest
-log_info "Installing VBox guest additions."
-sudo wget http://download.virtualbox.org/virtualbox/6.1.34/VBoxGuestAdditions_6.1.34.iso
-sudo mkdir /media/VBoxGuestAdditions
-sudo mount -o loop,ro VBoxGuestAdditions_6.1.34.iso /media/VBoxGuestAdditions
-sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run --nox11
-sudo rm VBoxGuestAdditions_6.1.34.iso
-sudo umount /media/VBoxGuestAdditions
-sudo rmdir /media/VBoxGuestAdditions
-log_info "VBox guest additions installed."
-
-
-
-# sudo wget http://download.virtualbox.org/virtualbox/4.3.8/VBoxGuestAdditions_4.3.8.iso
-# sudo mkdir /media/VBoxGuestAdditions
-# sudo mount -o loop,ro VBoxGuestAdditions_4.3.8.iso /media/VBoxGuestAdditions
-# sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
-# sudo rm VBoxGuestAdditions_4.3.8.iso
-# sudo umount /media/VBoxGuestAdditions
-# sudo rmdir /media/VBoxGuestAdditions
+# log_info "Installing VBox guest additions."
+# wget http://download.virtualbox.org/virtualbox/6.1.34/VBoxGuestAdditions_6.1.34.iso
+# mkdir /media/VBoxGuestAdditions
+# mount -o loop,ro VBoxGuestAdditions_6.1.34.iso /media/VBoxGuestAdditions
+# sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run --nox11
+# rm VBoxGuestAdditions_6.1.34.iso
+# umount /media/VBoxGuestAdditions
+# rmdir /media/VBoxGuestAdditions
+# log_info "VBox guest additions installed."
